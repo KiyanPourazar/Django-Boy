fe6b1b821d1074d286afa53cc25786453a2726d0
Kiyan <kiyanpourazar@gmail.com>
2026-08-07 23:35:09 +0330

    created html files: index(home), about and contact files

--- website/views.py
+++ website/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse,JsonResponse
+from django.shortcuts import render
 
 def index_home(request):
-    return HttpResponse('<h1>main</h1>')
+    return render(request, 'index.html')
 
 def index_about(request):
-    return HttpResponse('<h1>about</h1>')
+    return render(request, 'about.html')
 
 def index_contact(request):
-    return HttpResponse('<h1>cantact</h1>')
+    return render(request, 'contacts.html')
