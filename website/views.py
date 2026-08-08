4c6dbdab90e542ec07bb626db716fc191bdf5476
Kiyan <kiyanpourazar@gmail.com>
2026-08-09 01:27:05 +0330

    merge template with my project

--- website/views.py
+++ website/views.py
@@ -2,10 +2,10 @@ from django.http import HttpResponse,JsonResponse
 from django.shortcuts import render
 
 def index_home(request):
-    return render(request, 'index.html')
+    return render(request, 'website/index.html')
 
 def index_about(request):
-    return render(request, 'about.html')
+    return render(request, 'website/about.html')
 
 def index_contact(request):
-    return render(request, 'contacts.html')
+    return render(request, 'website/contact.html')
