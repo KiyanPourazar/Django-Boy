122e3cdfc6e475ce21b9dde0785c957363f94da1
Kiyan <kiyanpourazar@gmail.com>
2026-08-13 21:01:54 +0330

    add single page url with 404 error to blog

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse,JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 
 
@@ -13,7 +13,8 @@ def blog_single(request):
     context = {'title':'WARNING','content':'Oh shit here we go again'}
     return render(request,'blog/blog-single.html', context)
 
-def test(request):
-    posts = Post.objects.filter(status=1)
-    context = {'posts':posts}
+def test(request, pid):
+    # post = Post.objects.get(id=pid)
+    post = get_object_or_404(Post, pk=pid)
+    context = {'post':post}
     return render(request,'test.html',context)
