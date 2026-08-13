6a4506050c74c4ee91fb427e7bf09cc091cb3265
Kiyan <kiyanpourazar@gmail.com>
2026-08-13 17:08:54 +0330

    assign each post in database to a blog

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,19 @@
 from django.http import HttpResponse,JsonResponse
 from django.shortcuts import render
+from blog.models import Post
 
 
 # Create your views here.
 def blog_view(request):
-    return render(request,'blog/blog-home.html')
+    posts = Post.objects.filter(status=1)
+    context = {'posts':posts}
+    return render(request,'blog/blog-home.html',context)
 
 def blog_single(request):
     context = {'title':'WARNING','content':'Oh shit here we go again'}
     return render(request,'blog/blog-single.html', context)
+
+def test(request):
+    posts = Post.objects.filter(status=1)
+    context = {'posts':posts}
+    return render(request,'test.html',context)
