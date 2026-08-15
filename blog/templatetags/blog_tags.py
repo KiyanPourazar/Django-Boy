bf8bd0ca6cf0cedaf7f72ecca236a097aa1d1384
Kiyan <kiyanpourazar@gmail.com>
2026-08-16 00:04:31 +0330

    added inclusion tag

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -11,3 +11,7 @@ def get_posts():
 def snippet(value,arg=200):
     return value[:arg] + "..."
 
+@register.inclusion_tag('website/popularposts.html')
+def popularposts():
+    posts = Post.objects.filter(status=1).order_by('-published_date')[:2]
+    return {'posts':posts}
