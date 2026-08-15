2a96bab5b77df15bfab1ced2247e5f0f188ee372
Kiyan <kiyanpourazar@gmail.com>
2026-08-15 23:32:04 +0330

    added template tags to project

---
+++ blog/templatetags/blog_tags.py
@@ -0,0 +1,9 @@
+from django import template
+from blog.models import Post
+
+register = template.Library()
+
+@register.simple_tag(name='get_posts')
+def get_posts():
+    return Post.objects.filter(status=1).count()
+
