abebe2bd8156b5397ce4b3ec2b32c12d4c4139af
Kiyan <kiyanpourazar@gmail.com>
2026-08-15 23:49:13 +0330

    added filter to template tags

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -5,5 +5,9 @@ register = template.Library()
 
 @register.simple_tag(name='get_posts')
 def get_posts():
-    return Post.objects.filter(status=1).count()
+    return Post.objects.filter(status=1)
+
+@register.filter
+def snippet(value,arg=200):
+    return value[:arg] + "..."
 
