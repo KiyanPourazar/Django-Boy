d869def3cebf5b6c8182188405186e97f1cef96d
Kiyan <kiyanpourazar@gmail.com>
2026-08-17 01:10:29 +0330

    show slidebar categoris on blog pages

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from blog.models import Post
+from blog.models import Post, Category
 
 register = template.Library()
 
@@ -16,7 +16,17 @@ def snippet(value,arg=200):
 #     posts = Post.objects.filter(status=1).order_by('published_date')[:2]
 #     return {'posts':posts}
 
-@register.inclusion_tag('blog/blog-popular-posts.html')
+@register.inclusion_tag('blog/blog-latest-posts.html')
 def latest_posts(arg=3):
     posts = Post.objects.filter(status=1).order_by('published_date')[:arg]
     return {'posts':posts}
+
+@register.inclusion_tag('blog/blog-post-categories.html')
+def postcategories(arg=3):
+    posts = Post.objects.filter(status=1)
+    categories = Category.objects.all()
+    cat_dict = {}
+    for name in categories:
+        cat_dict[name]=posts.filter(category=name).count()
+
+    return {'categories':cat_dict}
