c0da4b4ce03182f7665170f27d59521ce291f7fb
Kiyan <kiyanpourazar@gmail.com>
2026-08-17 00:50:12 +0330

    added latest posts on the website

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -11,7 +11,12 @@ def get_posts():
 def snippet(value,arg=200):
     return value[:arg] + "..."
 
-@register.inclusion_tag('website/popularposts.html')
-def popularposts():
-    posts = Post.objects.filter(status=1).order_by('-published_date')[:2]
+# @register.inclusion_tag('website/popularposts.html')
+# def popularposts():
+#     posts = Post.objects.filter(status=1).order_by('published_date')[:2]
+#     return {'posts':posts}
+
+@register.inclusion_tag('blog/blog-popular-posts.html')
+def latest_posts(arg=3):
+    posts = Post.objects.filter(status=1).order_by('published_date')[:arg]
     return {'posts':posts}
