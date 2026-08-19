6966bc93373972effae75d12e4af66ad3b89c6e1
Kiyan <kiyanpourazar@gmail.com>
2026-08-20 00:03:56 +0330

    create a simple form

--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse,JsonResponse
 from django.shortcuts import render
+from website.models import Contact
 
 def index_home(request):
     return render(request, 'website/index.html')
@@ -11,8 +12,17 @@ def index_contact(request):
     return render(request, 'website/contact.html')
 
 def test_view(request):
-    context = {'name':'Kiyan','lastname':'Pourazar'}
-    return render(request, 'website/test.html', context)
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+        contact = Contact(name=name, email=email, subject=subject, message=message)
+        contact.save()
+
+
+
+    return render(request, 'website/test.html', {})
 
 def elements_view(request):
     return render(request, 'website/elements.html')
